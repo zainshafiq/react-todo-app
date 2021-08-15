afda67a8e998366ac056c2b5149e090e3503e615
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2021-08-14 18:29:13 -0600

    Update Counter.jsx

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,6 +3,19 @@ import './Counter.css'
 
 //Counter Class Component
 class Counter extends Component {
+
+    //Define the initial state in a constructor
+    //state => counter 0
+    constructor() {
+        super(); //Error 1
+        this.state = {
+            counter : 0
+        }
+
+        this.increment = this.increment.bind(this);
+    }
+
+
     render() {
         return (
             <div className="counter">
@@ -10,17 +23,19 @@ class Counter extends Component {
                 <br />
                 <button onClick={this.increment}>+1</button> {/* If its Javascript, you will need a curly braces */}
                 <br />
-                <span class="count">0</span>  
+                <span class="count">{this.state.counter}</span>  
             </div>
         )
     }
 
-    //Function inside a class (used to be outside)
-    increment() {
+    //Function (No need extend Component)
+    increment() { //Update state - counter++
         console.log('increment');
+        //this.state.counter++; // Bad Practice
+        this.setState({
+            counter: this.state.counter + 1
+        });
     }   
 }
-
-
 
 export default Counter
